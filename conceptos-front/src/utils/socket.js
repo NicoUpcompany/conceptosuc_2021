cbe2ed8104fd10063184a06c3dc54a2840994680
Nicolás <nicolas@upcompany.cl>
2021-11-12 16:43:11 -0300

    cambio url

--- conceptos-front/src/utils/socket.js
+++ conceptos-front/src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
-const Socket = io("http://localhost:8080/");
+// const Socket = io("http://localhost:8080/");
 // const Socket = io("https://cms.upwebinar.cl/");
-// const Socket = io("https://conceptosuc2021.upwebinar.cl/");
+const Socket = io("https://conceptosuc2021.upwebinar.cl/");
 
 export default Socket;
